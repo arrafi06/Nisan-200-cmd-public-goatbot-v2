b814b5ad2d086d86ebf0764fac7c7a175e7ea95c
arrafi06 <arraafi12344@gmail.com>
2025-04-18 23:02:31 +0600

    Update officialgc.js

--- scripts/cmds/officialgc.js
+++ scripts/cmds/officialgc.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   onStart: async function ({ api, event, threadsData, getLang, message }) {
-    const supportGroupThreadID = "28297135763266706"; // Replace with your support group thread ID
+    const supportGroupThreadID = "9571690832919492"; // Replace with your support group thread ID
     const botID = api.getCurrentUserID();
 
     try {
